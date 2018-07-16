abac54323fca7decad917e2d21a9d3ef908cbb43
hadifsy <41298778+hadifsy@users.noreply.github.com>
2018-07-16 18:26:21 +0300

    Add files via upload

---
+++ game_madlib1.py
@@ -0,0 +1,41 @@
+from random import randint
+
+def random_verb():
+    random_num = randint(0, 1)
+    if random_num == 0:
+        return "run"
+    else:
+        return "kayak"
+        
+def random_noun():
+    random_num = randint(0,1)
+    if random_num == 0:
+        return "sofa"
+    else:
+        return "llama"
+
+def word_transformer(word):
+    if word == "NOUN":
+        return random_noun()
+    elif word == "VERB":
+        return random_verb()
+    else:
+        return word[0]
+        
+def process_madlib(mad_lib):
+    processed = ""
+    index = 0
+    while index < len(mad_lib):
+        to_add=mad_lib[index:index+4]
+        x=word_transformer(to_add)
+        processed+=x
+        if len(x) ==1:
+            index+=1
+        else:
+            index+=4
+    return processed
+test_string_1 = "This is a good NOUN to use when you VERB your food"
+test_string_2 = "I'm going to VERB to the store and pick up a NOUN or two."
+print process_madlib(test_string_1)
+print process_madlib(test_string_2)
+
